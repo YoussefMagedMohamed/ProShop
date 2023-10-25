f66e970bca20113a3e8a6b36884b9b6bdc91a231
YoussefMagedMohamed <joeyoussef307@gmail.com>
2023-10-25 15:59:07 +0300

    NotFound Screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Screens/Home/Home";
 import ProductDetails from "./Screens/ProductDetails/ProductDetails";
+import NotFound from "./Screens/NotFound/NotFound";
 
 let routers = createBrowserRouter([
   {path:"/", element: <Layout/> , children: [
     {index: true , element: <Home/>} ,
     {path: "/productDetails/:id" , element: <ProductDetails/>} ,
+    {path: "*" , element: <NotFound/>} ,
   ]}
 ])
 
---
+++ src/Screens/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='p-5'>
+        <h1>Page Not Found</h1>
+        <p>We couldn't find what you were looking for.</p>
+        <p><strong>Please contact the owner of the site that linked you to the original URL and let them know their link is broken.</strong> </p>
+    </div>
+  )
+}
+
+export default NotFound
